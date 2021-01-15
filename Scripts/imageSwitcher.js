ac24a6699c797b38895d2ae7c559dbe194d8b150
Turner Bell <turner@tbell.us>
2021-01-14 23:36:59 -0500

    add gallery structure

--- Scripts/imageSwitcher.js
+++ Scripts/imageSwitcher.js
@@ -1,17 +1,1 @@
-﻿var currentIndex = 0,
-    items = $('.container div'),
-    itemAmt = items.length;
-
-function cycleItems() {
-    var item = $('.container div').eq(currentIndex);
-    items.hide();
-    item.css('display', 'inline-block');
-}
-
-var autoSlide = setInterval(function () {
-    currentIndex += 1;
-    if (currentIndex > itemAmt - 1) {
-        currentIndex = 0;
-    }
-    cycleItems();
-}, 300);
+﻿
